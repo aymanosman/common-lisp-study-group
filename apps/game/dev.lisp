4746884b3c927be4ebdb502f1902559166ef23d8
Ayman Osman <aymano.osman@gmail.com>
2022-04-25 17:53:50 +0100

    fix installation path

--- apps/game/dev.lisp
+++ apps/game/dev.lisp
@@ -1,7 +1,7 @@
 (ql:quickload "slynk")
 (slynk:create-server :port 4005 :dont-close t)
-;; git clone https://github.com/longlene/cl-raylib.git ~/.quicklisp/local-projects/cl-raylib
-(load "~/.quicklisp/local-projects/cl-raylib/cl-raylib.asd")
+;; git clone https://github.com/longlene/cl-raylib.git ~/quicklisp/local-projects/cl-raylib
+;; (load "~/quicklisp/local-projects/cl-raylib/cl-raylib.asd")
 (ql:quickload "cl-raylib")
 
 (load "main.lisp")
