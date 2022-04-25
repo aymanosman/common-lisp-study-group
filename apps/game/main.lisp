e2016a88a5c5e7961923084dc7f4530329b31409
Ayman Osman <aymano.osman@gmail.com>
2022-04-25 19:37:29 +0100

    game dev

--- apps/game/main.lisp
+++ apps/game/main.lisp
@@ -1,14 +1,49 @@
-(defpackage :main
-  (:use :cl :raylib)
+(defpackage #:main
+  (:use #:common-lisp #:raylib)
   (:export #:main))
 
-(in-package :main)
+(in-package #:main)
+
+(defvar boom)
+(defvar explosion)
+(defvar active nil)
+(defvar current-frame 0)
+(defvar explosions nil)
+
+(setf active nil)
+
+(defun draw-explosion (frame pos)
+  (let ((width (/ (texture-width explosion) 5))
+        (height (/ (texture-height explosion) 5)))
+    (multiple-value-bind (y x) (floor frame 5)
+      (draw-texture-rec explosion
+                        (make-rectangle :x (* x width)
+                                        :y (* y height)
+                                        :width width
+                                        :height height)
+                        pos
+                        +white+))))
 
 (defun game-loop ()
   (if (window-should-close) (return-from game-loop))
 
+  (when (and (is-mouse-button-pressed +mouse-left-button+) (not active))
+    (setf active t)
+    (let ((pos (get-mouse-position)))
+      (push (list 0 pos) explosions)))
+
+  (when active
+    (setf active nil)
+    (play-sound boom))
+
   (with-drawing
     (clear-background +raywhite+)
+    (dolist (e explosions)
+      (draw-explosion (first e) (second e))
+      (incf (first e))
+      (when (> (first e) 24)
+        (setf explosions (remove e explosions))))
+
     (draw-fps 10 10))
 
   (game-loop))
@@ -18,4 +53,6 @@
   (with-window (800 450 "raylib")
     (with-audio-device
       (set-target-fps 60)
+      (setf boom (load-sound "boom.wav"))
+      (setf explosion (load-texture "explosion.png"))
       (game-loop))))
